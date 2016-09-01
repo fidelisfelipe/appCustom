3106eb959f93a907a2744dd58b89059ad31e8ead
Fidelis <atosfiel@gmail.com>
2016-09-01 17:08:38 -0300

    init migration layout grocery

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@ angular.module('app', [
   // load your modules here
 // starting with the main module
   'main',
-  'authSocialBackand',
 ]).run(function ($rootScope) {
   $rootScope.title = 'App Custom';
 });
--- app/main/main.js
+++ app/main/main.js
@@ -2,101 +2,93 @@
 
 angular.module('main', [
   'ionic',
-  'ngCordova',
-  'ui.router',
+  'starter.controllers',
+  'starter.directives',
+  'starter.services',
+  'ionic-material',
+  'sir-accordion',
+  'ionicLazyLoad',
+  'ionicShop',
+  'ngMessages',
+  'ngStorage',
+  'ngSanitize'
   // TODO: load other modules selected during generation
 ])
-.config(function ($stateProvider, $urlRouterProvider) {
-
-  // ROUTING with ui.router
-  $urlRouterProvider.otherwise('auth/login');
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+ 
+ $ionicConfigProvider.navBar.alignTitle('center'); // Align title center
 
   $stateProvider
-    .state('main', {
-      url: '/main',
-      abstract: true,
-      templateUrl: 'main/templates/menu.html',
-      controller: 'AuthSocialBackandCtrl as ctrl',
-    }).state('main.home', {
-      url: '/home',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/home.html',
-          show: 'true'
-        }
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'main/templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.connection', {
+    url: '/connection',
+    views: {
+      'menuContent': {
+        templateUrl: 'main/templates/connection/connection.html',
+		controller: 'ConnectionCtrl'
       }
-    }).state('main.account', {
-      url: '/account',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/account.html',
-          controller: 'AuthSocialBackandCtrl as ctrl'
-        }
-      },
-    }).state('main.about', {
-      url: '/about',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/about.html',
-          show: 'true'
-        }
+    }
+  })
+
+  .state('app.iniscreen', {
+    url: '/iniscreen',
+    views: {
+      'menuContent': {
+        templateUrl: 'main/templates/iniscreen/iniscreen.html',
+		controller: 'IniscreenCtrl'
       }
-    }).state('main.debug', {
-      url: '/debug',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/debug.html',
-          controller: 'DebugCtrl as ctrl',
-          show: 'true'
-        }
-      }
-    }).state('main.link', {
-      url: '/link',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/link.html',
-          controller: 'LinkCtrl as ctrl',
-          show: 'true'
-        }
-      }
-    }).state('main.search', {
-      url: '/search',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/search.html',
-          controller: 'SearchCtrl as ctrl',
-          show: 'true'
-        }
-      }
-    }).state('main.setting', {
-      url: '/setting',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/setting.html',
-          controller: 'SettingCtrl as ctrl',
-          show: 'true'
-        }
-      }
-    }).state('main.upload', {
-      url: '/upload',
-      views: {
-        'pageContent': {
-          templateUrl: 'main/templates/upload.html',
-          controller: 'UploadCtrl as ctrl',
-          show: 'true'
-        }
-      }
-    });
-}).run(function ($rootScope, $state, $log, Main) {
-  $rootScope.$on('$stateChangeSuccess', function () {
-    $log.debug('run', $state.current.name);
-    if ($state.current.name === 'main.debug') {
-      Main.backendOnline();
     }
   });
-  Main.menus(function setMenus (menus) {
-    $rootScope.menus = menus;
-  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/connection');
 
-//set state dinamic
+}).run(function($ionicPlatform,$rootScope,$ionicPopup,$cordovaToast) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+	//----------------------
+	$rootScope.isCartIconVisible = false;
+	$rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+		if(toState.name=='app.dashboard' || toState.name=='app.products' || toState.name=='app.products-detail' || toState.name=='app.filter' || toState.name=='app.search' ){
+			$rootScope.isCartIconVisible = true;
+		}else{
+			$rootScope.isCartIconVisible = false;
+		}
+	});
+	//----------------------
+		$rootScope.tostMsg = function(msg){
+			$cordovaToast
+			.showShortTop(msg)
+			.then(function(success) {
+				// success
+			}, function (error) {
+				// error
+			});
+		}
+	//----------------------
+	 $rootScope.showAlert = function(msg) {	 
+		 $ionicPopup.alert({ 
+				title: 'Information', template: msg,
+				buttons: [ { text: 'OK',type: 'button-balanced', } ]
+			 });	
+		 };
+
+	//----------------------
+  });
 });
